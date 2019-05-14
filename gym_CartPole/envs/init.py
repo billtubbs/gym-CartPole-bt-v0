cebe089f29c7d2e49b325dfa28a8e5e456dd431c
Bill Tubbs <bill.tubbs@me.com>
2019-05-13 21:11:42 -0700

    Changed directory name

--- gym_CartPole/envs/init.py
+++
@@ -1,1 +0,0 @@
-from gym_CartPole_BT.envs.cartpole_bt_env import CartPoleBTEnv
--- gym_CartPole/init.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='CartPole-BT-v0',
-    entry_point='gym_CartPole_BT.envs:CartPoleBTEnv',
-)
