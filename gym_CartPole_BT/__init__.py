e7640540bfa712fb4b569f216c7cc72d5dd67c27
Bill Tubbs <bill.tubbs@me.com>
2019-05-16 18:48:16 -0700

    Added more environment combinations

--- gym_CartPole_BT/__init__.py
+++ gym_CartPole_BT/__init__.py
@@ -20,7 +20,7 @@ register(
     id='CartPole-BT-dH-v0',
     entry_point='gym_CartPole_BT.envs:CartPoleBTEnv',
     kwargs={
-        'description': "Basic cart-pendulum system with high disturbances",
+        'description': "Basic cart-pendulum system with large disturbances",
         'disturbances': 'high'
     }
 )
@@ -39,9 +39,31 @@ register(
     id='CartPole-BT-vH-v0',
     entry_point='gym_CartPole_BT.envs:CartPoleBTEnv',
     kwargs={
-        'description': "Basic cart-pendulum system with high variance in "
+        'description': "Basic cart-pendulum system with large variance in "
                        "initial state",
         'initial_state_variance': 'high'
+    }
+)
+
+register(
+    id='CartPole-BT-dL-vL-v0',
+    entry_point='gym_CartPole_BT.envs:CartPoleBTEnv',
+    kwargs={
+        'description': "Basic cart-pendulum system with small disturbances"
+                       "and small variance in initial state",
+        'initial_state_variance': 'low',
+        'disturbances': 'low'
+    }
+)
+
+register(
+    id='CartPole-BT-dH-vH-v0',
+    entry_point='gym_CartPole_BT.envs:CartPoleBTEnv',
+    kwargs={
+        'description': "Basic cart-pendulum system with large disturbances"
+                       "and large variance in initial state",
+        'initial_state_variance': 'high',
+        'disturbances': 'high'
     }
 )
 
@@ -53,6 +75,30 @@ register(
                        "distance -2 from goal",
         'initial_state': np.array([-1., 0., np.pi, 0.]),
         'goal_state': np.array([1., 0., np.pi, 0.])
+    }
+)
+
+register(
+    id='CartPole-BT-m2-dL-v0',
+    entry_point='gym_CartPole_BT.envs:CartPoleBTEnv',
+    kwargs={
+        'description': "Basic cart-pendulum system with initial state "
+                       "distance -2 from goal and small disturbances",
+        'initial_state': np.array([-1., 0., np.pi, 0.]),
+        'goal_state': np.array([1., 0., np.pi, 0.]),
+        'disturbances': 'low'
+    }
+)
+
+register(
+    id='CartPole-BT-m2-dH-v0',
+    entry_point='gym_CartPole_BT.envs:CartPoleBTEnv',
+    kwargs={
+        'description': "Basic cart-pendulum system with initial state "
+                       "distance -2 from goal and large disturbances",
+        'initial_state': np.array([-1., 0., np.pi, 0.]),
+        'goal_state': np.array([1., 0., np.pi, 0.]),
+        'disturbances': 'high'
     }
 )
 
