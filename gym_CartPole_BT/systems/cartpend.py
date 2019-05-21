12cf84257ffaf2dfbd04436c2f8b17aa6520aaf2
Bill Tubbs <bill.tubbs@me.com>
2019-05-20 19:24:48 -0700

    Added func to calculate A, B"

--- gym_CartPole_BT/systems/cartpend.py
+++ gym_CartPole_BT/systems/cartpend.py
@@ -1,7 +1,7 @@
 """
-Function to simulate the dynamics of a simple cart-pendulum system.
-Copied from MATLAB script provided by Steven L. Brunton as part of
-his Control Bootcamp series of YouTube videos.
+Functions to simulate the dynamics of a simple cart-pendulum system.
+Copied from MATLAB script provided by Steven L. Brunton as part of his
+Control Bootcamp series of YouTube videos.
 """
 
 import math
@@ -53,6 +53,48 @@ def cartpend_dydt(t, y, m=1, M=5, L=2, g=-10, d=1, u=0):
 
     return dy
 
+def cartpend_ss(m=1, M=5, L=2, g=-10, d=1, s=1):
+    """Calculates the linearized approximation of the cart-pendulum
+    system dynamics at either the vertical-up position (s=1) or
+    vertical-down position (s=-1).
+
+    Returns two arrays, A, B which are the system and input matrices
+    in the state-space system of differential equations:
+
+        x_dot = Ax + Bu
+
+    where x is the state vector, u is the control vector and x_dot
+    is the time derivative (dx/dt).
+
+    Args:
+        m (float): Mass of pendulum.
+        M (float): Mass of cart.
+        L (float): Length of pendulum.
+        g (float): Acceleration due to gravity.
+        d (float): Damping coefficient for friction between cart and
+            ground.
+        s (int): 1 for pendulum up position or -1 for down.
+
+    Returns:
+        dy (np.array): The time derivate of the state (dy/dt) as a
+            shape (4, ) array.
+    """
+
+    A = np.array([
+        [       0.,         1.,               0.,       0.],
+        [       0,        -d/M,           -m*g/M,       0.],
+        [       0.,         0.,               0.,       1.],
+        [       0., -s*d/(M*L), -s*(m+M)*g/(M*L),       0.]
+    ])
+
+    B = np.array([
+        [         0.],
+        [       1./M],
+        [         0.],
+        [ s*1./(M*L)]
+    ])
+
+    return A, B
 
 def run_unit_tests(show=False):
     """Runs a few unit tests to check that cartpend_dydt function
@@ -76,7 +118,7 @@ def run_unit_tests(show=False):
         4: [2.260914, 0.026066, 0.484470, -0.026480]
     }
 
-    u_test_values = {
+    test_values = {
         0: 0.,
         1: 0.,
         2: 1.,
@@ -95,7 +137,7 @@ def run_unit_tests(show=False):
         }
 
     t = 0.0
-    for i, u in u_test_values.items():
+    for i, u in test_values.items():
         y = np.array(y_test_values[i])
         dy_calculated = cartpend_dydt(t, y, m=m, M=M, L=L, g=g, d=d, u=u)
         dy_expected = np.array(expected_results[i])
@@ -103,4 +145,31 @@ def run_unit_tests(show=False):
         if show:
             print(f"Test {i} success")
 
+    # K values below calculated with MATLAB script from
+    # Steven L. Brunton's Control Bootcamp videos
+    test_values = {
+        5: 1,  # Pendulum up position
+        6: -1  # Pendulum down position
+    }
+    expected_results = {
+        5: (np.array([[0.0,   1.0,   0.0,   0.0],
+                      [0.0,  -0.2,   2.0,   0.0],
+                      [0.0,   0.0,   0.0,   1.0],
+                      [0.0,  -0.1,   6.0,   0.0]]),
+            np.array([[ 0.0], [ 0.2], [ 0.0], [ 0.1]])),
+        6: (np.array([[0.0,   1.0,   0.0,   0.0],
+                      [0.0,  -0.2,   2.0,   0.0],
+                      [0.0,   0.0,   0.0,   1.0],
+                      [0.0,   0.1,  -6.0,   0.0]]),
+            np.array([[ 0.0], [ 0.2], [ 0.0], [-0.1]]))
+    }
+    for i, s in test_values.items():
+        A_calculated, B_calculated = cartpend_ss(m=m, M=M, L=L, g=g, d=d, s=s)
+        A_expected, B_expected = expected_results[i]
+        assert np.isclose(A_calculated, A_expected).all(), f"Test {i} failed"
+        assert np.isclose(B_calculated, B_expected).all(), f"Test {i} failed"
+        if show:
+            print(f"Test {i} success")
+
+# For now, let's run unit-tests every time this module is imported
 run_unit_tests()
