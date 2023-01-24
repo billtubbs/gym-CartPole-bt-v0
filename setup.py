964e10f7b48f285303f048576857f7b4830809b4
billtubbs <bill.tubbs@me.com>
2023-01-23 18:36:22 -0800

    Typo in keyword

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='gym_CartPole_BT',
       version='0.0.2',
-      auther=['Bill Tubbs'],
+      author=['Bill Tubbs'],
       url='https://github.com/billtubbs/gym-CartPole-bt-v0/',
       py_modules=[],
       install_requires=['gym', 'pyglet']
