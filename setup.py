5ab82b9b97534a43de432e251b6e4cd6b56dc042
billtubbs <bill.tubbs@me.com>
2023-01-23 18:34:27 -0800

    Fixed install problems
    Problem with setuptools, See https://stackoverflow.com/a/72547402/1609514

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(name='gym_CartPole_BT',
-      version='0.0.1',
-      install_requires=['gym']  # Add any other dependencies here
+      version='0.0.2',
+      auther=['Bill Tubbs'],
+      url='https://github.com/billtubbs/gym-CartPole-bt-v0/',
+      py_modules=[],
+      install_requires=['gym', 'pyglet']
 )
